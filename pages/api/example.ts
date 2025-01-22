b15976c9d1d38fe7401ff3a8125bcda611568ecd
Muhammad Raihan Akram Ramadhan <raihan.of2008@gmail.com>
2025-01-22 11:20:54 +0700

    create example.ts and [id].ts in users folder

---
+++ pages/api/example.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method, query, body, headers} = req;
+
+    res.status(200).json({
+        method, 
+        query,
+        body,
+        headers
+    });
+}
